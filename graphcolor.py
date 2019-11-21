61bfb8c317d959136d7cadfefe35246d924d4ba3
Emily Shoji <eshoji1@lion.lmu.edu>
2019-11-21 15:56:01 -0800

    add graph color images

--- graphcolor.py
+++ graphcolor.py
@@ -1,22 +1,36 @@
-def color_nodes(graph):
-    color_map = {}
-    # Consider nodes in descending degree
-    for node in sorted(graph, key=lambda x: len(graph[x]), reverse=True):
-        neighbor_colors = set(color_map.get(neigh) for neigh in graph[node])
-        color_map[node] = next(
-            color for color in range(len(graph)) if color not in neighbor_colors
-        )
-    return color_map
+colors = ['Red', 'Blue', 'Green', 'Yellow', 'Black']
+
+states = ['Andhra', 'Karnataka', 'TamilNadu', 'Kerala']
+
+neighbors = {}
+neighbors['Andhra'] = ['Karnataka', 'TamilNadu']
+neighbors['Karnataka'] = ['Andhra', 'TamilNadu', 'Kerala']
+neighbors['TamilNadu'] = ['Andhra', 'Karnataka', 'Kerala']
+neighbors['Kerala'] = ['Karnataka', 'TamilNadu']
+
+colors_of_states = {}
 
 
-if __name__ == '__main__':
-    graph = {
-        'a': list('bcd'),
-        'b': list('ac'),
-        'c': list('abdef'),
-        'd': list('ace'),
-        'e': list('cdf'),
-        'f': list('ce')
-    }
-    print(color_nodes(graph))
-    # {'c': 0, 'a': 1, 'd': 2, 'e': 1, 'b': 2, 'f': 2}
+def promising(state, color):
+    for neighbor in neighbors.get(state):
+        color_of_neighbor = colors_of_states.get(neighbor)
+        if color_of_neighbor == color:
+            return False
+
+    return True
+
+
+def get_color_for_state(state):
+    for color in colors:
+        if promising(state, color):
+            return color
+
+
+def main():
+    for state in states:
+        colors_of_states[state] = get_color_for_state(state)
+
+    print(colors_of_states)
+
+
+main()
