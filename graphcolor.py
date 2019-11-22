84a96d078a04b0ac89f6e604ef12f78342971631
Emily Shoji <eshoji1@lion.lmu.edu>
2019-11-22 02:00:00 -0800

    update graphcolor.py and add activity1 solutions

--- graphcolor.py
+++ graphcolor.py
@@ -1,30 +1,35 @@
-doctors = ['Doctor 1', 'Doctor 2', 'Doctor 3', 'Doctor 4', 'Doctor 5']
+doctors = ['Doctor 1', 'Doctor 2', 'Doctor 3', 'Doctor 4']
 
-times = [str(n) for n in range(8, 19)]
+times = []
 
-neighbors = {}
-neighbors['8'] = ['9']
-neighbors['9'] = ['8', '10']
-neighbors['10'] = ['9', '11']
-neighbors['11'] = ['10', '12']
-neighbors['12'] = ['11', '13']
-neighbors['13'] = ['12', '14']
-neighbors['14'] = ['13', '15']
-neighbors['15'] = ['14', '15']
-neighbors['16'] = ['15', '17']
-neighbors['17'] = ['16', '18']
-neighbors['18'] = ['17', '19']
-neighbors['19'] = ['18']
+for t in range(7, 20):
+    times.append(str(t))
+
+appt_graph = {}
+
+appt_graph['7'] = ['8', '11']
+appt_graph['8'] = ['9', '7', '12']
+appt_graph['9'] = ['10', '8', '13']
+appt_graph['10'] = ['11', '9', '14']
+appt_graph['11'] = ['12', '10', '7']
+appt_graph['12'] = ['13', '11', '8']
+appt_graph['13'] = ['14', '12', '9']
+appt_graph['14'] = ['15', '13', '10']
+appt_graph['15'] = ['16', '14']
+appt_graph['16'] = ['17', '15']
+appt_graph['17'] = ['18', '16']
+appt_graph['18'] = ['19', '17']
+appt_graph['19'] = ['18']
+
 
 appointments = {}
 
 
 def promising(time, doctor):
-    for neighbor in neighbors.get(time):
-        color_of_neighbor = appointments.get(neighbor)
-        if color_of_neighbor == doctor:
+    for neighbor in appt_graph.get(time):
+        doctor_for_neighbor_appt = appointments.get(neighbor)
+        if doctor_for_neighbor_appt == doctor:
             return False
-
     return True
 
 
@@ -47,3 +52,58 @@ if __name__ == '__main__':
             appointments_per_doctor[doctor] = [time]
 
     print(appointments_per_doctor)
+
+
+# Insert Code
+
+# Insert Code
+
+# Insert Code
+  # Insert Code
+
+# Insert Code
+
+appt_graph['7'] = ['8', '11']
+appt_graph['8'] = ['9', '7', '12']
+appt_graph['9'] = ['10', '8', '13']
+appt_graph['10'] = ['11', '9', '14']
+appt_graph['11'] = ['12', '10', '7']
+appt_graph['12'] = ['13', '11', '8']
+appt_graph['13'] = ['14', '12', '9']
+appt_graph['14'] = ['15', '13', '10']
+appt_graph['15'] = ['16', '14']
+appt_graph['16'] = ['17', '15']
+appt_graph['17'] = ['18', '16']
+appt_graph['18'] = ['19', '17']
+appt_graph['19'] = ['18']
+
+
+# Insert Code
+
+def promising(time, doctor):
+    # Insert Code
+    doctor_for_neighbor_appt = appointments.get(neighbor)
+    if doctor_for_neighbor_appt == doctor:
+            # Insert Code
+           # Insert Code
+
+
+def get_doctor_for_appt(time):
+    # Insert Code
+    if promising(time, doctor):
+            # Insert Code
+
+
+if __name__ == '__main__':
+    # Insert Code
+    # Insert Code
+
+    # Insert Code
+
+    for time, doctor in appointments.items():
+        if doctor in appointments_per_doctor.keys():
+            appointments_per_doctor[doctor].append(time)
+        else:
+            appointments_per_doctor[doctor] = [time]
+
+    # Insert Code
