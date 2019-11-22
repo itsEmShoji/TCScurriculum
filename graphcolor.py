f669ab1cf3a710a8c5d62e5a6b18ca9ed7328ba1
Emily Shoji <eshoji1@lion.lmu.edu>
2019-11-21 22:18:08 -0800

    adapt graphcolor.py to schscheduling problem and add images

--- graphcolor.py
+++ graphcolor.py
@@ -1,36 +1,49 @@
-colors = ['Red', 'Blue', 'Green', 'Yellow', 'Black']
+doctors = ['Doctor 1', 'Doctor 2', 'Doctor 3', 'Doctor 4', 'Doctor 5']
 
-states = ['Andhra', 'Karnataka', 'TamilNadu', 'Kerala']
+times = [str(n) for n in range(8, 19)]
 
 neighbors = {}
-neighbors['Andhra'] = ['Karnataka', 'TamilNadu']
-neighbors['Karnataka'] = ['Andhra', 'TamilNadu', 'Kerala']
-neighbors['TamilNadu'] = ['Andhra', 'Karnataka', 'Kerala']
-neighbors['Kerala'] = ['Karnataka', 'TamilNadu']
+neighbors['8'] = ['9']
+neighbors['9'] = ['8', '10']
+neighbors['10'] = ['9', '11']
+neighbors['11'] = ['10', '12']
+neighbors['12'] = ['11', '13']
+neighbors['13'] = ['12', '14']
+neighbors['14'] = ['13', '15']
+neighbors['15'] = ['14', '15']
+neighbors['16'] = ['15', '17']
+neighbors['17'] = ['16', '18']
+neighbors['18'] = ['17', '19']
+neighbors['19'] = ['18']
 
-colors_of_states = {}
+appointments = {}
 
 
-def promising(state, color):
-    for neighbor in neighbors.get(state):
-        color_of_neighbor = colors_of_states.get(neighbor)
-        if color_of_neighbor == color:
+def promising(time, doctor):
+    for neighbor in neighbors.get(time):
+        color_of_neighbor = appointments.get(neighbor)
+        if color_of_neighbor == doctor:
             return False
 
     return True
 
 
-def get_color_for_state(state):
-    for color in colors:
-        if promising(state, color):
-            return color
+def get_doctor_for_appt(time):
+    for doctor in doctors:
+        if promising(time, doctor):
+            return doctor
 
 
-def main():
-    for state in states:
-        colors_of_states[state] = get_color_for_state(state)
+if __name__ == '__main__':
+    for time in times:
+        appointments[time] = get_doctor_for_appt(time)
 
-    print(colors_of_states)
+    appointments_per_doctor = {}
 
+    for time, doctor in appointments.items():
+        if doctor in appointments_per_doctor.keys():
+            appointments_per_doctor[doctor].append(time)
+        else:
+            appointments_per_doctor[doctor] = [time]
 
-main()
+    print(appointments_per_doctor)
